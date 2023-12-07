5c982d8e65c5ac38764ee2b84e437e888df3ca3b
Dirk Meyer <65828797+DIRKGM@users.noreply.github.com>
2023-12-07 16:25:10 +0100

    Protocoll

--- Wetter App Test/Model/WeatherManager.swift
+++ Wetter App Test/Model/WeatherManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// Protocoll wird genutzt um die Daten ausserhalb dieser Datei zu nutzen
 protocol WeatherManagerDelegate {
     func didUpdateWeather(weather: WeatherModel)
     func didFailWithError(error: Error)
